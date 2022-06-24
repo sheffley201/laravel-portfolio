7ddbf32a3e8e962b8bfdc769f68ac29ead59627a
Spencer Heffley <spheffley@yahoo.com>
2022-06-23 22:23:37 -0400

    Changed some more styling and added project cards

---
+++ resources/views/components/project-card.blade.php
@@ -0,0 +1,2 @@
+<div class="h-40 w-60 rounded-lg bg-zinc-800 overflow-hidden shadow-lg">
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,23 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow-sm sm:rounded-lg text-gray-100">
-                You're logged in!
+                <div class='mx-auto w-fit'>
+                    <h2 class='text-2xl mb-5'>Check out my projects!</h2>
+                    <div class='grid grid-cols-3 gap-6'>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                        <x-project-card></x-project-card>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
